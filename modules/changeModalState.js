884e8cc1d7ebca469704f7f2a7c835bd3d638a8c
Oleksandr Harnytskyi <garnitsky@gmail.com>
2021-07-27 23:18:46 +0300

    forms1

---
+++ modules/changeModalState.js
@@ -0,0 +1,25 @@
+import checkNumInputs from "./checkNumInputs";
+
+const changeModalState = (state) => {
+
+    const windowForm = document.querySelectorAll('.balcon_icons_img'),
+        windowWith = document.querySelector('#width'),
+        windowHeight = document.querySelector('#height'),
+        windowType = document.querySelector('#view_type'),
+        windowProfile = document.querySelectorAll('.checkbox');
+
+    checkNumInputs('#width');
+    checkNumInputs('#height');
+
+
+    windowForm.forEach((item, i) => {
+        item.addEventListener('click', () => {
+            state.form = i;
+            console.log(state);
+        });
+
+    });
+
+};
+
+export default changeModalState;
